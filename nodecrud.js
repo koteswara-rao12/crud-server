cb00983d072f7612e32d1110b240e943ab00ad1e
koteswararao <koteswara.rao@softsuave.com>
2020-03-17 15:29:26 +0530

    server

--- nodecrud.js
+++ nodecrud.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql');
 const express = require('express');
+var cors = require('cors');
 var app = express();
 const bodyparser = require('body-parser');
-
+app.use(cors())
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
     extended: false
 }));
 
+app.options('*', cors())
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,7 +19,7 @@ var mysqlConnection = mysql.createConnection({
     multipleStatements: true
 });
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", 'http://localhost:4200');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
@@ -55,7 +57,7 @@ app.get('/user/:id', (req, res) => {
 });
 
 //Delete an employees
-app.delete('/user/:id', (req, res) => {
+app.delete('/remove/:id',cors(), (req, res,next) => {
     mysqlConnection.query('DELETE FROM user WHERE id = ?', [req.params.id], (err, rows, fields) => {
         if (!err)
             res.send('Deleted successfully.');
@@ -83,14 +85,18 @@ app.post('/user/add', (req, res) => {
 }); 
 
 //Update an employees
-app.put('/user/:id', (req, res) => {
-    let user = req.body[0];
-    console.log(" hello");
-    console.log(req.body[0]);
-    var sql ="UPDATE user SET  name= ?,email = ?,age= ?,location  = ? WHERE id = ? ";
-    var query= mysqlConnection.query(sql, [user.name,user.email, user.age, user.location,req.params.id],  (err, rows, fields) => {
+app.put('/user/:id', cors(), (req, res,next) => {
+    
+    let user = req.body;
+    console.log(" hello name");
+    //console.log(req.body.name);
+    var sql ='UPDATE user SET  name= ?,email = ?,age= ?,location  = ? WHERE id = ?' ;
+    var query= mysqlConnection.query(sql, [user.name,user.email, user.age, user.location,req.params.id],(err, rows, fields) => {
         if (!err)
+        {
+        console.log("server update");
             res.send('Updated successfully');
+        }
         else
             console.log(err);
 
